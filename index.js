a1bb7355c02482a0e23629e10cf25f88e5a72346
rohanpaspallu <paspallu.rohan@gmail.com>
2022-02-21 15:41:03 -0500

    setting up the port

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res)=>{
-    res.send("hello world")
+    res.send("hello world !")
 })
 
 app.get('/api/courses', (req, res)=>{
     res.send([1,2,3])
 })
 
-app.listen(3000, ()=> console.log('listening on 3000 ....'))
+const port = process.env.PORT || 3000
+app.listen(port, ()=> console.log(`listening on ${port} ....`))
