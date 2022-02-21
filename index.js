50f39149e33aee67b853f19a788d0fcbf316cfdd
rohanpaspallu <paspallu.rohan@gmail.com>
2022-02-21 15:35:39 -0500

    first api created

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-fafd
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.send("hello world")
+})
+
+app.get('/api/courses', (req, res)=>{
+    res.send([1,2,3])
+})
+
+app.listen(3000, ()=> console.log('listening on 3000 ....'))
