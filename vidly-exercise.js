717dfe33b26a6ed909af3c9bc622e6bba13cb449
rohanpaspallu <paspallu.rohan@gmail.com>
2022-02-23 15:53:41 -0500

    completed the vidly exercise

--- vidly-exercise.js
+++ vidly-exercise.js
@@ -1,27 +1,74 @@
-const joi = require('joi')
+const Joi = require('joi')
 const express = require('express')
 const app = express()
 
-app.get('/api/courses',(req, res)=>{
+app.use(express.json())
+const genres = [
+    {id: 1, name:"genre 1"},
+    {id: 2, name:"genre 2"}
+]
+
+app.get('/',(req, res)=>{
+    res.send("welcome to vidly")
+})
+
+app.get('/api/genres', (req, res) => {
+    res.send(genres)
+})
+
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g=>g.id===parseInt(req.params.id))
+
+    if (!genre) return res.status(404).send("genre not found")
+    res.send(genre)
 
 })
 
-app.get('/api/courses/:id',(req, res)=>{
-    
+app.post('/api/genres', (req, res) => {
+    // res.send(req.body.name)
+    const {error} = validateGenre(req.body)
+
+    if(error) return res.status(400).send(error.details[0].message)
+
+    const genre = {
+        id: genres.length +1,
+        name: req.body.name}
+
+    genres.push(genre)
+    res.send(genres)
 })
 
-app.post('/api/courses',(req, res)=>{
+
+
+app.put('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g=>g.id===parseInt(req.params.id))
+    if (!genre) return res.status(404).send("genre not found")
+
+    const {error} = validateGenre(req.body)
+
+    if(error) return res.status(400).send(error.details[0].message)
+
+    genre.name = req.body.name
+
+    res.send(genres)
 
 })
 
-app.put('/api/courses/:id',(req, res)=>{
-    
+app.delete('/api/genres/:id', (req, res) => {
+
+    const genre = genres.find(g=>g.id===parseInt(req.params.id))
+    if (!genre) return res.status(404).send("genre not found")
+
+    const index = genres.indexOf(genre)
+    genres.splice(index, 1)
+
+    res.send(genres)
 })
 
-app.delete('/api/courses/:id',(req, res)=>{
-    
-})
+function validateGenre(genre) {
+    const schema = { name: Joi.string().min(3).required() }
+    return Joi.validate(genre, schema)
+}
 
-const port = process.env.PORT|| 3001
-console.log(port)
-app.listen(port, ()=>console.log("connected to port 3001...."))
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`listening on ${port} ....`))
