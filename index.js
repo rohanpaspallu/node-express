8817166998e9c71a700b53d2db46e3db08bfcbe8
rohanpaspallu <paspallu.rohan@gmail.com>
2022-02-24 18:41:15 -0500

    moved the custom middlewares to new files

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const Joi = require('joi')
+const logger = require('./logger')
+const authenticator = require('./authenticator')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
 
-app.use(function (req, res, next) {
-    console.log('Logging...')
-    next()
-})
+app.use(logger)
 
-app.use(function (req, res, next) {
-    console.log('authenticating...')
-    next()
-})
+app.use(authenticator)
 
 const courses = [
     { id: 1, name: 'course1' },
