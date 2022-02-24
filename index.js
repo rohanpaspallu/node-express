f93666d0c31861308c0c947ca4134ccb7c7a6fcb
rohanpaspallu <paspallu.rohan@gmail.com>
2022-02-24 18:58:50 -0500

    some more middlewares

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const helmet = require('helmet')
+const morgan = require('morgan')
 const Joi = require('joi')
 const logger = require('./logger')
 const authenticator = require('./authenticator')
@@ -5,6 +7,10 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(helmet())
+app.use(morgan('tiny'))
 
 app.use(logger)
 
