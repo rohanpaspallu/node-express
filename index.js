326bd9ebc60f3ff2d3c93008aaf5b11b03f7768b
rohanpaspallu <paspallu.rohan@gmail.com>
2022-02-24 19:20:35 -0500

    added config folder to access different environments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('config')
 const helmet = require('helmet')
 const morgan = require('morgan')
 const Joi = require('joi')
@@ -6,11 +7,22 @@ const authenticator = require('./authenticator')
 const express = require('express')
 const app = express()
 
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
+console.log(`app: ${app.get('env')}`)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(helmet())
-app.use(morgan('tiny'))
+
+//configuration
+console.log('Application name:'+ config.get("name"))
+console.log('Mail server:'+ config.get("mail.host"))
+
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'))
+    console.log('morgan enabled')
+}
+
 
 app.use(logger)
 
