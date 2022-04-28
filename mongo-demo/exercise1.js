acf3e168f2776171b4e9f1ef9ceef15a8e6c4b48
rohanpaspallu <paspallu.rohan@gmail.com>
2022-04-28 16:36:20 -0400

    exercise 1

---
+++ mongo-demo/exercise1.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/mongo-exercises')
+    .then(()=> console.log("connected to mongo-exercises"))
+    .catch((err)=>console.log(err[0].message))
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    isPublished: Boolean,
+    date: {type: Date, default: Date.now},
+    price: Number
+})
+
+const Course = mongoose.model('Course', courseSchema)
+
+async function getCourses(){
+    const courses = await Course
+        .find({tags: 'backend', isPublished: true})
+        .sort({name:1})
+        .select({name:1, author:1})
+
+    console.log(courses)
+}
+
+getCourses()
