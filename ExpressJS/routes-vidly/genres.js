04dbf7728d7be7cf2024bb39eec5ed2d32aea0a3
rohanpaspallu <paspallu.rohan@gmail.com>
2022-05-06 14:41:13 -0400

    referencing documents i.e. normalization

--- ExpressJS/routes-vidly/genres.js
+++ ExpressJS/routes-vidly/genres.js
@@ -3,16 +3,7 @@ const mongoose = require('mongoose')
 const router = express.Router()
 const Joi = require('joi')
 
-const genresSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50
-    }
-})
 
-const Genre = mongoose.model('Genre', genresSchema)
 
 
 router.get('/', async (req, res) => {
@@ -68,9 +59,6 @@ router.delete('/:id', async(req, res) => {
     res.send(genre)
 })
 
-function validateGenre(genre) {
-    const schema = { name: Joi.string().min(3).required() }
-    return Joi.validate(genre, schema)
-}
+
 
 module.exports = router
