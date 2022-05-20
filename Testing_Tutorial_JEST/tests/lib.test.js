688fac66e35022ce2c9c5cf40b560f9111e90e47
rohanpaspallu <paspallu.rohan@gmail.com>
2022-05-20 16:09:40 -0400

    added a few test cases for absolute testing

--- Testing_Tutorial_JEST/tests/lib.test.js
+++ Testing_Tutorial_JEST/tests/lib.test.js
@@ -1,3 +1,18 @@
-test('Our first test', () => {
+const lib = require('../lib')
+test('absolute - should return a positive number if input is positive', () => {
+    const result = lib.absolute(1)
+    expect(result).toBe(1)
+    // throw new Error("error")
+})
+
+test('absolute - should return a positive number if input is negative', () => {
+    const result = lib.absolute(-1)
+    expect(result).toBe(1)
+    // throw new Error("error")
+})
+
+test('absolute - should return a positive number if input is negative', () => {
+    const result = lib.absolute(0)
+    expect(result).toBe(0)
     // throw new Error("error")
 })
