10da2db0c00d2c89a94cbb0d885c56c81631d147
rohanpaspallu <paspallu.rohan@gmail.com>
2022-05-25 15:28:39 -0400

    created a fake/mock function and integrated the testing

--- Testing_Tutorial_JEST/tests/lib.test.js
+++ Testing_Tutorial_JEST/tests/lib.test.js
@@ -1,5 +1,5 @@
 const lib = require('../lib')
-
+const db = require('../db')
 describe('absolute', ()=>{
     it('should return a positive number if input is positive', () => {
         const result = lib.absolute(1)
@@ -84,5 +84,17 @@ describe('register user', ()=>{
 
         expect(result).toMatchObject({username:'rohan'})
         expect(result.id).toBeGreaterThan(0)
+    })
+})
+
+describe('apply discount', ()=>{
+    it('should apply 10% discount if customer has more than 10 points',()=>{
+        db.getCustomerSync = function(customerId){
+            console.log('fake reading customer...')
+            return {id: customerId, points: 20}
+        }
+        const order = {customerId: 1, totalPrice: 10}
+        lib.applyDiscount(order)
+        expect(order.totalPrice).toBe(9)
     })
 })
