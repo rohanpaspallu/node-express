2f8fc7b02552c7262e37adac6055591dd0183cf5
rohanpaspallu <paspallu.rohan@gmail.com>
2022-05-31 12:39:18 -0400

    final commit to the project

--- ExpressJS/tests/integration/genres.test.js
+++ ExpressJS/tests/integration/genres.test.js
@@ -1,4 +1,5 @@
 
+const { default: mongoose } = require('mongoose')
 const request = require('supertest')
 const {Genre} = require('../../models/genres')
 const {User} = require('../../models/users')
@@ -43,6 +44,12 @@ describe('/api/genres', ()=>{
 
         it('should return 404 if invalid id is passed', async()=>{
             const res = await request(server).get('/api/genres/1')
+            expect(res.status).toBe(404)
+        })
+
+        it('should return 404 if no genre with given id exists', async()=>{
+            const id = mongoose.Types.ObjectId()
+            const res = await request(server).get('/api/genres/'+ id)
             expect(res.status).toBe(404)
         })
     })
